b0d7b335cf59bb5ca18f2b5af11cf599552df44d
Gabriela Regina de Souza <122756066+GabrielaSouzaDev@users.noreply.github.com>
2026-05-29 13:17:34 -0300

    correcao arquivo selenium

---
+++ Selenium.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+url = "https://www.amazon.com.br/Echo-Pop-Cor-Preta/dp/B09WXVH7WK/th=1"
+
+driver.get(url)
+print(driver.page_source)  # imprime o HTML da página recebida
+time.sleep(5)
+
+inteiro = driver.find_element(By.CLASS_NAME, "a-price-whole").text
+decimal = driver.find_element(By.CLASS_NAME, "a-price-fraction").text
+
+print(f'O preço do produto está em R${inteiro},{decimal}')
+
+driver.quit()
